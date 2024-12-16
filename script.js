0fd058f358fe427edb6abb4c9850143ad7e3eb88
Manish Kumar Patel <maneeshkurmii@gmail.com>
2024-12-16 09:35:01 +0530

    Uploding complete clone project at once.

---
+++ script.js
@@ -0,0 +1,6 @@
+function myFunctionH1() {
+    document.getElementById('myFunIDH1').innerHTML = "We're Sorry 🙇"
+}
+function myFunction() {
+    document.getElementById('myFunID').innerHTML = "Still this page is on development mode and this button won't do any thing also this is a copy of a website made for learning purpose of HTMl, CSS & Javascript. Please visit later for better experience <br>Last worked on this clone project 12/16/2024 09:07:07 AM Monday ⏩<br> THANK YOU 🙏"
+}
